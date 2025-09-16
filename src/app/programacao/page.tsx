008937758a35ee2ba0d4a6be9cfe39a4540df3b3
Everson <igor@EVERSON.localdomain>
2025-09-16 14:52:05 -0400

    atualizacao de texto 2

--- src/app/programacao/page.tsx
+++ src/app/programacao/page.tsx
@@ -184,11 +184,7 @@ export default function Programacao() {
                     <div className="bg-[#2767702B] px-5 py-10 rounded-lg">
                         <Image src={"/asteristico-azul.svg"} width={30} height={30} alt="Viver de IA"/>
                         <span className="bg-[var(--azul-neon)] rounded-full text-sm px-3 py-1 mt-5 mb-4 inline-block">15h44 – 16h14</span>
-                        <p className="text-white font-bold text-sm mb-2">Q&A com CEO Grupo Digital</p>
-                        <div className="block">
-                            <p className="font-bold text-[var(--azul-neon)] uppercase">Yago Martins</p>
-                            <p className="font-bold text-[var(--azul-neon)] uppercase">Rafael Milagre</p>
-                        </div>
+                        <p className="text-white font-bold text-sm">Q&A com CEO Grupo Digital</p>
                     </div>
                     <div className="bg-[#2767702B] px-5 py-10 rounded-lg">
                         <Image src={"/asteristico-azul.svg"} width={30} height={30} alt="Viver de IA"/>
